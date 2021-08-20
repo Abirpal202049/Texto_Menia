66b948dc3f0c4c567a5fe3f3585cdb0e8d184302
Abir Pal <abirpal282002@gmail.com>
2021-08-20 17:04:26 +0530

    Version 1.1 Search Function Introduce

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 
 function App() {
   const [mode, setMode] = useState('light')
   const [text, setText] = useState('Enable Dark Mode')
+  const [alert, setAlert] = useState('Light Mode Enabled Successfully')
   const toggle = ()=>{
     if(mode === 'dark'){
       setMode('light');
       setText('Enable Dark Mode')
       document.body.style.backgroundColor = 'white'
       document.body.style.color = '#1f1f1f'
+      setAlert('Light Mode Enabled Successfully')
+      
     }
     else{
       setMode('dark');
       setText('Enable Light Mode')
       document.body.style.backgroundColor = '#1f1f1f'
       document.body.style.color = 'white'
+      setAlert('Dark Mode Enabled Successfully')
     }
   }
 
 
   return (
     <>
-      <Navbar titleText = "Texto_Menia" mode ={mode} change={toggle} toggletext = {text}/> {/* [titleText = "Texto_Menia"] is called as Props  */}
+      <Navbar titleText = "Texto_Menia" mode ={mode} change={toggle} toggletext = {text}/> 
+      <Alert mode ={mode} text = {alert}/>
       <Textform boxheading="Enter Your Text Below To Analyse" mode = {mode} change={toggle}/>
       
     </>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react' // useState is a Hook(React Hooks)
 import PropTypes from 'prop-types'
+// import reactDom from 'react-dom';
+import './Transform.css';
 
 
 
@@ -24,6 +26,10 @@ export default function Textform(props) {
         let newtext = '';
         setText(newtext);
     }
+    const RandomText = ()=>{
+        let randtext = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, adipisci quas explicabo optio exercitationem earum ea incidunt, voluptas vitae consectetur pariatur facere ullam nisi distinctio eaque sequi odio mollitia omnis. Recusandae, eum eius. Aliquam quae ipsa velit dolorem. At harum ipsa nostrum consequatur optio non esse, itaque pariatur excepturi assumenda molestias facere quisquam tempore iusto fugit placeat ut maiores cum voluptates ratione, saepe odio perspiciatis. Optio placeat qui blanditiis architecto necessitatibus quis enim iure impedit molestiae labore repellendus minus doloremque sequi dolor, nihil vitae eum facilis soluta voluptates autem officia. Nemo illo ipsum earum magni veniam voluptatibus debitis dicta quae.`
+        setText(randtext);
+    }
 
 
     const handelonchangeClick =(event) => {
@@ -32,9 +38,39 @@ export default function Textform(props) {
     }
 
 
+    // Search Funchinility
+    const [searchtext, setSearchtext] = useState("")
+    const [decision, setDecision] = useState('Present')
+    const handelonchangeClicksearch =(event) => {
+        // console.log("handelonchangeClick function is triggered now");
+        setSearchtext(event.target.value);
+        // console.log(searchtext);
+    }
+    const searchup = () =>{
+        let arr = Text.split(" ");
+        let key =  searchtext;
+        let flag = -2;
+        for (let i = 0; i < arr.length; i++) {
+            if(key === arr[i]){
+                flag = i;
+                break;
+            }
+            else{
+                flag = -1;
+            }
+        }
+        if (flag === -1) {
+            setDecision("Not Present");
+        }
+        else{
+            setDecision(`Present At Position ${flag + 1}`);
+        }
 
+    }
 
     
+
+
 
     return (
         <div>
@@ -44,22 +80,37 @@ export default function Textform(props) {
                     <textarea className="form-control" id="text" rows="8" value={Text} onChange={handelonchangeClick} placeholder="Enter your text here ..."></textarea>
                 </div>
                 
-                <button type="button" className={`btn btn-${props.mode === 'dark' ? 'primary' : 'dark'} mx-2`} onClick={Clicktouppercase} >Convert To Uppercase</button>
-                <button type="button" className={`btn btn-${props.mode === 'dark' ? 'primary' : 'dark'} mx-2`} onClick={Clicktolowercase}>Convert To Lowercase</button>
-                <button type="button" className={`btn btn-${props.mode === 'dark' ? 'primary' : 'dark'} mx-2`} onClick={Clicktoclear}>Clear Text</button>
+                <button type="button" className={`btn btn-${props.mode === 'dark' ? 'primary' : 'dark'} mx-1 my-1`} onClick={Clicktouppercase} >Convert To Uppercase</button>
+                <button type="button" className={`btn btn-${props.mode === 'dark' ? 'primary' : 'dark'} mx-1 my-1`} onClick={Clicktolowercase}>Convert To Lowercase</button>
+                <button type="button" className={`btn btn-${props.mode === 'dark' ? 'primary' : 'dark'} mx-1 my-1`} onClick={Clicktoclear}>Clear Text</button>
+                <button type="button" className={`btn btn-${props.mode === 'dark' ? 'primary' : 'dark'} mx-1 my-1`} onClick={RandomText}>Generate Random Text</button>
                 <hr />
-                <div className="container my-3">
-                    <h2>Your Text Summary</h2>
+
+                <div className="container my-3 meniaflex">
                     <div className="container">
-                    <h6>Charecter With Space : <span>{Text.length} Charecter</span> </h6>
-                    <h6>Charecter : <span>{Text === '' ? "0" : (Text.length - ((Text === '' ? "0" :(Text.split(' ').length)) - 1))} Charecter</span> </h6>
-                    <h6>Words : <span>{Text === '' ? "0" :(Text.split(' ').length)} Words</span></h6>
+                    <h2 className="meniaflexHeading">Your Text Summary</h2>
+                    <div className="container my-3">
+                        <h6>Charecter With Space : <span>{Text.length} Charecter</span> </h6>
+                        <h6>Charecter : <span>{Text === '' ? "0" : (Text.length - ((Text === '' ? "0" :(Text.split(' ').length)) - 1))} Charecter</span> </h6>
+                        <h6>Words : <span>{Text === '' ? "0" :(Text.split(' ').length)} Words</span></h6>
+                    </div>
+                    </div>
+                    <div className="container">
+                        <h2 className="meniaflexHeading searchheading"><label htmlFor="searchword">Enter the word you want to Search </label></h2>
+                        <form className="d-flex my-3">
+                            
+                            <input rows="1" value={searchtext} onChange={handelonchangeClicksearch} type="search" className="form-control me-2"  id="searchword" placeholder="Enter the word you want to find from the above text"/>
+                            
+                            <button type="button" onClick={searchup} className={`btn btn-${props.mode === 'dark' ? 'success' : 'dark'} mx-2 ${searchtext === "" ? "disabled" : ""}`} >Search</button>
+                        </form>
+                        <h5>The word is {decision}</h5>
                     </div>
                 </div>
                 <hr />
                 <div className="container my-3">
                     <h2>Preview Text</h2>
-                    <p>{Text === '' ? "Type Your Text Here To Preview...." : Text}</p>
+                    <p>{Text === '' ? "Type Your Text Above To Preview Here...." : Text}</p>
+                    
                 </div>
             </div>
         </div>
